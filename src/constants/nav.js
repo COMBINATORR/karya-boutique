b68510844d84af8daf6972f982c4140659c76cff
COMBINATORR <combinatorr@users.noreply.github.com>
2026-08-06 02:44:36 +0500

    Add Features link to site navigation

--- src/constants/nav.js
+++ src/constants/nav.js
@@ -1,6 +1,7 @@
 export const NAV = [
   { href: '#categories', key: 'nav.categories' },
   { href: '#details', key: 'nav.details' },
+  { href: '#features', key: 'nav.features' },
   { href: '#about', key: 'nav.about' },
   { href: '#location', key: 'nav.location' },
 ]
