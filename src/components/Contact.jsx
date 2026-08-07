d210d9f527af4b24200ae497152a805a7a158ebd
COMBINATORR <combinatorr@users.noreply.github.com>
2026-08-07 19:05:51 +0500

    Contact gallery: drop 06.webp

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -22,11 +22,10 @@ const SLIDES = [
   '/images/contact/03.webp',
   '/images/contact/04.webp',
   '/images/contact/05.webp',
-  '/images/contact/06.webp',
 ]
 
 /** Bump when gallery assets change */
-const SLIDE_V = 2
+const SLIDE_V = 3
 
 const SLIDE_MS = 5500
 
